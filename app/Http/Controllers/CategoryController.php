fcdb6361962ac25696a14b3bf3062dcb45ec5a4c
Nguyen nhan <nguyennnhanlovemyself@gmail.com>
2020-05-23 15:54:56 +0700

    new

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,13 +7,59 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function listCategory(){
-        $categories=Category::paginate(1);
-        // show with condition: start from D
-        //$categories = category::where("category_name","LIKE","D%")->get();
-        // dd($categories);
+    public function listCategory()
+    {
+        $categories =Category::paginate(1);
         return view("category.list", [
             "categories" => $categories
         ]);
+
+    }
+
+    public function newCategory()
+    {
+        return view("category.new");
+    }
+    public function saveCategory(Request $request){
+        $request->validate([
+            "category_name"=> "required|string|min:6|unique:categories"
+        ]);
+        try{
+            Category::create([
+                "category_name"=> $request->get("category_name")
+            ]);
+        }catch (\Exception $exception){
+            return redirect()->back();
+        }
+        return redirect()->to("/list-category");
+    }
+
+    public function editCategory($id){
+        $category = Category::findOrFail($id);
+        return view("category.edit",["category"=>$category]);
+    }
+
+    public function updateCategory($id,Request $request){
+        $category = Category::findOrFail($id);
+        $request->validate([
+            "category_name"=> "required|min:6|unique:categories,category_name,{$id}"
+        ]);
+        try {
+            $category->update([
+                "category_name"=> $request->get("category_name")
+            ]);
+        }catch (\Exception $exception){
+            return redirect()->back();
+        }
+        return redirect()->to("/list-category");
+    }
+
+    public function deleteCategory($id){
+        $category = Category::findOrFail($id);
+        try{
+            $category->delete();
+        }catch (\Exception $exception){
+        }
+        return redirect()->to("/list-category");
     }
 }
--- resources/views/category/new.blade.php
+++ resources/views/category/new.blade.php
@@ -1,1 +1,44 @@
-<?php
+@extends("layout")
+@section("title","new-category")
+@section("content")
+    <div class="row clearfix">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <div class="card">
+                <div class="header">
+                    <h2>
+                        NEW CATEGORY
+                    </h2>
+                    <ul class="header-dropdown m-r--5">
+                        <li class="dropdown">
+                            <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                <i class="material-icons">more_vert</i>
+                            </a>
+                            <ul class="dropdown-menu pull-right">
+                                <li><a href="javascript:void(0);">Action</a></li>
+                                <li><a href="javascript:void(0);">Another action</a></li>
+                                <li><a href="javascript:void(0);">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div class="body">
+                    <form role="form" action="{{url("save-category")}}" method="post">
+                        @method("POST")
+                        @csrf
+                        <label>Category Name</label>
+                        <div class="form-group">
+                            <div class="form-line">
+                                <input class="form-control @error("category_name") is-invalid @enderror" type="text" name="category_name" placeholder="Enter Name"/>
+                                @error("category_name")
+                                <span class="error invalid-feedback">{{$message}}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <br>
+                        <button type="submit" class="btn btn-primary m-t-15 waves-effect">SUBMIT</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
