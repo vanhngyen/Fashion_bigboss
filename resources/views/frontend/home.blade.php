d4afec1c6a8e65ed78ac6efccc60f31139d7aa35
Nguyen nhan <nguyennnhanlovemyself@gmail.com>
2020-06-21 19:53:08 +0700

    nn

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -647,7 +647,7 @@
                             <div class="block2-pic hov-img0">
                                 <img src="{{$m->getImage()}}" alt="IMG-PRODUCT">
 
-                                <a href="#"
+                                <a href="/cart/add/{product}"
                                    class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
                                     Quick View
                                 </a>
@@ -684,7 +684,7 @@
                             <div class="block2-pic hov-img0">
                                 <img src="{{$w->getImage()}}" alt="IMG-PRODUCT">
 
-                                <a href="#"
+                                <a href="/cart/add/{product}"
                                    class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
                                     Quick View
                                 </a>
@@ -721,7 +721,7 @@
                             <div class="block2-pic hov-img0">
                                 <img src="{{$s->getImage()}}" alt="IMG-PRODUCT">
 
-                                <a href="#"
+                                <a href="/cart/add/{product}"
                                    class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
                                     Quick View
                                 </a>
@@ -758,7 +758,7 @@
                             <div class="block2-pic hov-img0">
                                 <img src="{{$b->getImage()}}" alt="IMG-PRODUCT">
 
-                                <a href="#"
+                                <a href="/cart/add/{product}"
                                    class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
                                     Quick View
                                 </a>
@@ -795,7 +795,7 @@
                             <div class="block2-pic hov-img0">
                                 <img src="{{$w->getImage()}}" alt="IMG-PRODUCT">
 
-                                <a href="#"
+                                <a href="/cart/add/{product}"
                                    class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1">
                                     Quick View
                                 </a>
