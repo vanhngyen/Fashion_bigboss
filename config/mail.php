789ec9442faaa5bcac61cfe426cdbca1143ac5af
Nguyen nhan <nguyennnhanlovemyself@gmail.com>
2020-06-21 15:47:25 +0700

    a

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
-            'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('MAIL_USERNAME'),
-            'password' => env('MAIL_PASSWORD'),
+            'host' => 'smtp.gmail.com',//env('MAIL_HOST', 'smtp.mailgun.org'),
+            'port' => '587',
+            'encryption' => 'tls',
+            'username' => 'thangsteam3@gmail.com',
+            'password' => 'fpngnzjthekvoasn',
             'timeout' => null,
             'auth_mode' => null,
         ],
